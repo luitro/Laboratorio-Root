aa1cce33caa0c2cb1081020aa5e16a24253b49fd
Luigi Trotta <luigitrotta@Air-di-Luigi.lan>
2024-10-25 12:35:58 +0200

    Fine seconda prova

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "TMath.h"
 #include "TROOT.h"
 #include "TRandom.h"
+#include "TFile.h"
 #include "particle.hpp"
 
 int main() {
@@ -16,12 +17,13 @@ int main() {
   gRandom->SetSeed();
 
   int N = 120;
-  Particle EventParticles[N] = 0;
+  Particle EventParticles[N];  // sistemare il messaggio findparticle
   Double_t phi;
   Double_t theta;
   Double_t p;
   Double_t x;
   int firstempty = 100;
+  double PiK = 0.13957 + 0.49367;
 
   TH1F *h1 = new TH1F("h1", "Particle types", 7, 0, 1E7);
   TH1F *h2 = new TH1F("h2", "Polar angles", 1E3, 0, 2 * TMath::Pi());
@@ -29,10 +31,25 @@ int main() {
   TH1F *h4 = new TH1F("h4", "Impulse", 1E4, 0, 10);
   TH1F *h5 = new TH1F("h5", "Transverse impulse", 1E4, 0, 10);
   TH1F *h6 = new TH1F("h6", "Energy", 1E4, 0, 10);
-  TH1F *h7 = new TH1F("h7", "Invariant mass", 1E5, 0, 10);
+  TH1F *h7 = new TH1F("h7", "Invariant mass between all particles", 1E5, 0, 10);
+  TH1F *h8 = new TH1F(
+      "h8", "Invariant mass between concordant charge particles", 1E5, 0, 10);
+  TH1F *h9 =
+      new TH1F("h9", "Invariant mass between disconcordant charge particles",
+               1E5, 0, 10);
+  TH1F *h10 = new TH1F(
+      "h10", "Invariant mass between concordant charge Pi and K particles", 1E5,
+      0, 10);
+  TH1F *h11 = new TH1F(
+      "h11", "Invariant mass between disconcordant charge Pi and K particles",
+      1E5, 0, 10);
+  TH1F *h12 = new TH1F(
+      "h12", "Benchmark", 1E5, 0, 10);
+  TH1F *h13 = new TH1F(
+      "h13", "h11-h10", 1E5, 0, 10);
 
   for (int i = 0; i < 1E5; ++i) {
-    for (Int_t j = 0; j < 100; ++i) {
+    for (Int_t j = 0; j < 100; ++j) {
       phi = gRandom->Uniform(0., 2 * TMath::Pi());
       theta = gRandom->Uniform(0., TMath::Pi());
       p = gRandom->Exp(1.);
@@ -57,6 +74,8 @@ int main() {
         EventParticles[firstempty + 1].SetIndex("Pi-");
         EventParticles[j].Decay2body(EventParticles[firstempty],
                                      EventParticles[firstempty + 1]);
+        h12->Fill(
+            EventParticles[firstempty].InvMass(EventParticles[firstempty + 1]));
         firstempty += 2;
       } else {
         EventParticles[j].SetIndex("K*");
@@ -64,6 +83,8 @@ int main() {
         EventParticles[firstempty + 1].SetIndex("Pi+");
         EventParticles[j].Decay2body(EventParticles[firstempty],
                                      EventParticles[firstempty + 1]);
+        h12->Fill(
+            EventParticles[firstempty].InvMass(EventParticles[firstempty + 1]));
         firstempty += 2;
       }
 
@@ -74,8 +95,48 @@ int main() {
       h5->Fill(sqrt(pow(p, 2) + pow(EventParticles[j].GetPx(), 2)));
       h6->Fill(EventParticles[j].Energy());
     }
-    for (auto& const i : EventParticles){
-      h7->Fill(i.InvMass())
+
+    for (int a = 0; a < N; ++a) {
+      if (EventParticles[a].GetIndex() != -1 &&
+          EventParticles[a].GetIndex() != 6) {
+        for (int b = a + 1; b < N; ++b) {
+          if (EventParticles[b].GetIndex() != -1 &&
+              EventParticles[b].GetIndex() != 6) {
+            Double_t c = EventParticles[a].InvMass(EventParticles[b]);
+            h7->Fill(c);
+            if (((EventParticles[a].GetIndex() + EventParticles[b].GetIndex()) %
+                 2) == 0) {
+              h8->Fill(c);
+              if (EventParticles[a].GetMass() + EventParticles->GetMass() ==
+                  PiK)
+                h10->Fill(c);
+            } else {
+              h9->Fill(c);
+              if (EventParticles[a].GetMass() + EventParticles->GetMass() ==
+                  PiK)
+                h11->Fill(c);
+            }
+          }
+        }
+      }
     }
+    h10->Sumw2();
+    h11->Sumw2();
+    h13->Add(h11, h10, 1, -1); 
   }
+  std::cout<< h12->GetMean()<< '\t' << h12->GetRMS() << std::endl;
+  TFile *outputFile = new TFile("histograms.root", "RECREATE");
+  h1->Write();
+  h2->Write();
+  h3->Write();
+  h4->Write();
+  h5->Write();
+  h6->Write();
+  h7->Write();
+  h8->Write();
+  h9->Write();
+  h10->Write();
+  h11->Write();
+  h12->Write();
+  outputFile->Close();
 }
